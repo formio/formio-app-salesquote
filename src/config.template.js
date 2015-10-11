8a0e1dc090e37db00b0b63cb6bda3e2e48a53f66
Travis Tidwell <codertravist@gmail.com>
2015-10-11 11:23:27 -0500

    Update config.template.js

--- src/config.template.js
+++ src/config.template.js
@@ -1,5 +1,10 @@
 angular.module('formioSalesQuoteApp').constant('AppConfig', {
+{% if domain %}
+  appUrl: '{{ domain }}',
+  apiUrl: '{{ domain }}',
+{% else %}
   appUrl: '{{ protocol }}://{{ path }}.{{ host }}',
   apiUrl: '{{ protocol }}://api.{{ host }}',
+{% endif %}
   company: 'Acme Inc.'
 });
